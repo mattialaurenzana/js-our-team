354ed68e38139449cd107ff6821899c92474881d
mattia laurenzana <mattilaure@gmail.com>
2022-01-10 14:48:09 +0100

    completamento BONUS e ottimizzazione codice

--- js/script.js
+++ js/script.js
@@ -30,23 +30,57 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+  function createOutputHtml (object){
+    teamContainer.innerHTML += `  
+    <div class="team-card">
+    <div class="card-image">
+      <img
+        src="img/${object.image}"
+        alt="${object.name}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${object.name}</h3>
+      <p>${object.role}</p>
+    </div>
+  </div>` 
+  }
   
 const teamContainer = document.querySelector('.team-container');
   for(let i=1; i < team.length; i++){
     const member = team[i];  //salvo in una variabile il membro attuale
     
-    //aggiungo al contenitore le card dinamicamente
-    teamContainer.innerHTML += `  
-    <div class="team-card">
-    <div class="card-image">
-      <img
-        src="img/${member.image}"
-        alt="${member.name}"
-      />
-    </div>
-    <div class="card-text">
-      <h3>${member.name}</h3>
-      <p>${member.role}</p>
-    </div>
-  </div>` 
+  //richiamo la funzione per aggiungere l'oggetto all'html
+  createOutputHtml(member);
   };
+
+
+  /***************************************************************************/
+  //BONUS
+
+  /*Utilizziamo gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team:
+  cliccando sul pulsante “add” viene creato un nuovo oggetto, il quale viene inserito nell’array iniziale,
+   e viene stampata una nuova card con tutte le informazioni inserite dall’utente.*/
+  const button = document.getElementById('addMemberButton');
+
+  button.addEventListener('click',function(){
+
+    //salvo in variabili i valori inseriti dall'utente come input
+    const name_ = document.getElementById('name').value;
+    const image = document.getElementById('image').value;
+    const role = document.getElementById('role').value;
+
+    const newMember = {  //viene creato un nuovo oggetto a partire dalle informazioni inserite dall'utente
+      name : name_,
+      role : role,
+      image : image
+    }
+  
+    team.push(newMember); //inserisco il nuovo oggetto creato all'interno dell'array contenente tutti i membri del team
+
+    createOutputHtml(newMember);
+    
+
+    
+  });
